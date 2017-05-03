3c19cc519fb5540f5f84fc3e71047377ad9af428
Michal <m14lickiewicz@gmail.com>
2017-05-03 08:30:27 +0200

    Add comments, finish work.

--- event-capture-etc/src/js/script.js
+++ event-capture-etc/src/js/script.js
@@ -1,6 +1,7 @@
 const divs = document.querySelectorAll('div');
 
 function logText(e) {
+    // Stop propagation of persistent events.
     e.stopPropagation();
     console.log(this.classList.value)
 }
@@ -8,6 +9,10 @@ function logText(e) {
 
 divs.forEach(function (div) {
     div.addEventListener('click', logText, {
+        // Register the event handler for the BUBBLING phase. It's default value.
         capture: false
+
+        // Register the event handler for the CAPTURING phase.
+        // capture: true
     })
 });
